79a121b59bcc2e9919c3ba9830bb74b8f62cfb61
ThiconZ <xennma@outlook.com>
2020-07-01 17:14:52 -0400

    Make winmd reference absolute
    Removed unused namespaces and mae the winmd reference absolute. PR #1

--- PSO2 Thin Launcher/Launcher.cs
+++ PSO2 Thin Launcher/Launcher.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Windows.Management.Deployment;
 using Windows.ApplicationModel;
+using Windows.Management.Deployment;
 
 namespace PSO2_Thin_Launcher
 {
